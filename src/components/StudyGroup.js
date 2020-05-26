509f6d14ae22c773a46a3affb3ea9e78386c9038
sophiaw789 <sophialiwon@gmail.com>
2020-05-26 15:34:08 -0400

    bootstrap and container

---
+++ src/components/StudyGroup.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class StudyGroup extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Course Study Group Search</h1>
+            </div>
+        )
+    }
+}
+
+export default StudyGroup
