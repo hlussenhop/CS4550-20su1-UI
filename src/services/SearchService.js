f296c5a1ce7ce9bb69aa46bf3e063ef2c3aadf44
sophiaw789 <sophialiwon@gmail.com>
2020-05-26 15:25:02 -0400

    component/service folders

---
+++ src/services/SearchService.js
@@ -0,0 +1,41 @@
+const createCourse = (course) => 
+    fetch("https://wbdv-generic-server.herokuapp.com/api/001452943/courses", {
+        method: 'POST',
+        body: JSON.stringify(course),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+
+const updateCourse = (courseId, course) =>
+    fetch("https://wbdv-generic-server.herokuapp.com/api/001452943/courses/" + courseId, {
+        method: 'PUT',
+        body: JSON.stringify(course),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+const deleteCourse = (courseId) => 
+    fetch("https://wbdv-generic-server.herokuapp.com/api/001452943/courses/" + courseId, {
+        method: 'DELETE'
+    })
+        .then(response => response.json())
+
+const findCourseById = (courseId) => { }
+
+const findAllCourses = () => 
+    fetch("https://wbdv-generic-server.herokuapp.com/api/001452943/courses")
+        .then(response => response.json())
+
+
+export default {
+    createCourse,
+    deleteCourse,
+    findCourseById,
+    findAllCourses,
+    updateCourse
+}
