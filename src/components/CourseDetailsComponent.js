86e076cb48da9535e5149cd5a951161edcf55159
bureaurat <hlussenhop@gmail.com>
2020-05-29 22:28:14 -0400

    course details

--- src/components/CourseDetailsComponent.js
+++ src/components/CourseDetailsComponent.js
@@ -1,88 +1,34 @@
 import React from "react";
+import CourseRowComponent from "./CourseRowComponent";
+import SectionCardComponent from "./SectionCardComponent";
 
-const CourseDetailsComponent = () =>
-    <div className="container">
-        <div className="card mb-3">
-            <div className="card-header"><b>CS3500 Object-Oriented Design</b></div>
-            <div className="card-body">
-                <p className="card-text">Presents a comparative approach to object-oriented programming and design. Discusses the concepts of object, class, meta-class, message, method, inheritance, and genericity. Reviews forms of polymorphism in object-oriented languages. Contrasts the use of inheritance and composition as dual techniques for software reuse: forwarding vs. delegation and subclassing vs. subtyping. Fosters a deeper understanding of the principles of object-oriented programming and design including software components, object-oriented design patterns, and the use of graphical design notations such as UML (unified modeling language). Basic concepts in object-oriented design are illustrated with case studies in application frameworks and by writing programs in one or more object-oriented languages.
-                </p>
-            </div>
-        </div>
+export default class CourseDetailsComponent extends React.Component {
 
-        <p1><b>Sections</b></p1>
-        <div className="row row-cols-1 row-cols-md-2">
-            <div className="col mb-4">
-                <div className="card bg-light">
-                    <div className="card-header"><b>CRN 10425</b></div>
+
+    render() {
+        //console.log(this.props)
+        return (
+            <div className="container">
+                <div className="card mb-3">
+                    <div className="card-header"><b>{this.props.location.state.course.class.name}</b></div>
                     <div className="card-body">
-                        <div className="row">
-                        <div className="col-6">
-                            <h6 className="card-text course-professor">Professor</h6>
-                            <h6 className="card-text course-time">Times</h6>
-                            <h6 className="card-text course-days">Days</h6>
-                            <h6>Location</h6>
-                        </div>
-                        <div className="col-6">
-                            <h6 className="card-text text-muted">Amit Shesh</h6>
-                            <h6 className="card-text course-time text-muted">9:50am-11:30pm</h6>
-                            <h6 className="card-text course-days text-muted">Tuesday, Friday</h6>
-                            <h6 className="card-text course-location text-muted">Behrakis Health Sciences Center 310</h6>
-                            <button href="#" className="btn btn-success float-right card-link">Enroll</button>
-                            {/*<button href="#" className="btn btn-danger float-right card-link">UnEnroll</button>*/}
-                        </div>
-                        </div>
+                        <p className="card-text">{this.props.location.state.course.class.desc}
+                        </p>
                     </div>
                 </div>
-            </div>
-            <div className="col mb-4">
-                <div className="card bg-light">
-                    <div className="card-header"><b>CRN 10425</b></div>
-                    <div className="card-body">
-                        <div className="row">
-                            <div className="col-6">
-                                <h6 className="card-text course-professor">Professor</h6>
-                                <h6 className="card-text course-time">Times</h6>
-                                <h6 className="card-text course-days">Days</h6>
-                                <h6>Location</h6>
-                            </div>
-                            <div className="col-6">
-                                <h6 className="card-text text-muted">Amit Shesh</h6>
-                                <h6 className="card-text course-time text-muted">9:50am-11:30pm</h6>
-                                <h6 className="card-text course-days text-muted">Tuesday, Friday</h6>
-                                <h6 className="card-text course-location text-muted">Behrakis Health Sciences Center 310</h6>
-                                <button href="#" className="btn btn-success float-right card-link">Enroll</button>
-                                {/*<button href="#" className="btn btn-danger float-right card-link">UnEnroll</button>*/}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-                <div className="col mb-4">
-                    <div className="card bg-light">
-                        <div className="card-header"><b>CRN 10425</b></div>
-                        <div className="card-body">
-                            <div className="row">
-                                <div className="col-6">
-                                    <h6 className="card-text course-professor">Professor</h6>
-                                    <h6 className="card-text course-time">Times</h6>
-                                    <h6 className="card-text course-days">Days</h6>
-                                    <h6>Location</h6>
-                                </div>
-                                <div className="col-6">
-                                    <h6 className="card-text text-muted">Amit Shesh</h6>
-                                    <h6 className="card-text course-time text-muted">9:50am-11:30pm</h6>
-                                    <h6 className="card-text course-days text-muted">Tuesday, Friday</h6>
-                                    <h6 className="card-text course-location text-muted">Behrakis Health Sciences Center 310</h6>
-                                    <button href="#" className="btn btn-success float-right card-link">Enroll</button>
-                                    {/*<button href="#" className="btn btn-danger float-right card-link">UnEnroll</button>*/}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-        </div>
 
-    </div>
+                <p1><b>Sections</b></p1>
+                <div className="row row-cols-1 row-cols-md-2">
+                    {
+                        this.props.location.state.course.sections.map(section =>
+                            <SectionCardComponent
+                                section={section}/>
+                        )
+                    }
 
-export default CourseDetailsComponent
+                </div>
+
+            </div>
+        )
+    }
+}
--- src/components/StudyGroupComponent.js
+++ src/components/StudyGroupComponent.js
@@ -3,7 +3,9 @@ import CourseContainer from '../containers/CourseContainer'
 import CourseDetailsComponent from "./CourseDetailsComponent";
 import CourseSearchComponent from "./CourseSearchComponent";
 import NavBarComponent from "./NavBarComponent";
-import { BrowserRouter, Link, Route } from "react-router-dom";
+import {BrowserRouter, Link, Route} from "react-router-dom";
+import CourseTableComponent from "./CourseTableComponent";
+import CourseRowComponent from "./CourseRowComponent";
 
 class StudyGroupComponent extends React.Component {
 
@@ -15,21 +17,17 @@ class StudyGroupComponent extends React.Component {
                     <Route
                         path="/search"
                         exact={true}
-                        component={CourseContainer} />
+                        component={CourseContainer}/>
 
                     <Route
                         path='/search/:keyword'
                         exact={true}
-                        component={CourseContainer} />
+                        component={CourseContainer}/>
 
-                    <Route path='search/:course_id/details/'
-                        exact={true}
-                        component={CourseDetailsComponent} />
-
-                {/*For test*/}
-                   <Route path='/details/'
-                    exact={true}
-                    component={CourseDetailsComponent} />
+                    <Route path='/details/:id'
+                           exact={true}
+                           render={(props) => <CourseRowComponent {...props} someProp={100}/>}
+                           component={CourseDetailsComponent}/>
                 </div>
             </BrowserRouter>
         )
