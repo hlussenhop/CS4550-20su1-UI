09ddbd25565b78a723e28104ba1bf9998b97efbd
sophiacrennan <crennan.s@husky.neu.edu>
2020-06-17 04:11:00 -0700

    Store user information in state

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -4,45 +4,71 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import LoginComponentCSS from "./LoginComponent.css";
 import {Link} from "react-router-dom";
 
-const LoginComponent = () =>
-    <div className="login">
-        <div className="d-flex justify-content-center h-100">
-            <div className="card login_card">
-                <div className="card-header">
-                    <h3>Sign In</h3>
-                </div>
-                <div className="card-body">
-                    <form>
-                        <div className="input-group form-group">
-                            <div className="input-group-prepend">
+class LoginComponent extends React.Component {
+
+    state = {
+        email: '',
+        password: ''
+    }
+
+    render() {
+        return (
+            <div className="login">
+                <div className="d-flex justify-content-center h-100">
+                    <div className="card login_card">
+                        <div className="card-header">
+                            <h3>Sign In</h3>
+                        </div>
+                        <div className="card-body">
+                            <form>
+                                <div className="input-group form-group">
+                                    <div className="input-group-prepend">
                                 <span className="input-group-text">
                                     <FontAwesomeIcon icon={faUser}/>
                                 </span>
-                            </div>
-                            <input type="text" className="form-control" placeholder="Username"/>
-                        </div>
-                        <div className="input-group form-group">
-                            <div className="input-group-prepend">
+                                    </div>
+                                    <input type="text"
+                                           className="form-control"
+                                           placeholder="Email"
+                                           onChange={(event) =>
+                                               this.setState({email: event.target.value})}/>
+                                </div>
+
+                                <div className="input-group form-group">
+                                    <div className="input-group-prepend">
                                 <span className="input-group-text">
                                     <FontAwesomeIcon icon={faKey}/>
                                 </span>
+                                    </div>
+                                    <input type="password"
+                                           className="form-control"
+                                           placeholder="Password"
+                                           onChange={(event) =>
+                                               this.setState({role: event.target.value})}/>
+                                </div>
+
+                                <div className="form-group">
+                                    <Link to={"/search"}>
+                                        <button className="btn float-right login_btn"
+                                        // onClick={() => this.props.findUserId(this.state.email, this.state.password)}
+                                        >
+                                            Sign Up
+                                        </button>
+                                    </Link>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="card-footer">
+                            <div className="d-flex justify-content-center links">
+                                Don't have an account?<a href="/signup">Sign Up</a>
                             </div>
-                            <input type="password" className="form-control" placeholder="Password"/>
                         </div>
-                        <div className="form-group">
-                            <Link to={"/search"}>
-                                <input type="submit" value="Login" className="btn float-right login_btn"/>
-                            </Link>
-                        </div>
-                    </form>
-                </div>
-                <div className="card-footer">
-                    <div className="d-flex justify-content-center links">
-                        Don't have an account?<a href="/signup">Sign Up</a>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+        )
+    }
+}
+
 
 export default LoginComponent
--- src/components/SignUpComponent.js
+++ src/components/SignUpComponent.js
@@ -1,56 +1,125 @@
 import React from "react";
 import {faUser, faKey} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import LoginComponentCSS from "./LoginComponent.css";
 import {Link} from "react-router-dom";
 
-const SignUpComponent = () =>
-    <div className="login">
-        <div className="d-flex justify-content-center h-100">
-            <div className="card login_card">
-                <div className="card-header">
-                    <h3>Sign Up</h3>
-                </div>
-                <div className="card-body">
-                    <form>
-                        <div className="input-group form-group">
-                            <div className="input-group-prepend">
+class SignUpComponent extends React.Component {
+
+    state = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        role: ''
+    }
+
+    render() {
+        return (
+            <div className="login">
+                <div className="d-flex justify-content-center h-100">
+                    <div className="card login_card">
+                        <div className="card-header">
+                            <h3>Sign Up</h3>
+                        </div>
+                        <div className="card-body">
+                            <form>
+                                <div className="input-group form-group">
+                                    <div className="input-group-prepend">
                                 <span className="input-group-text">
                                     <FontAwesomeIcon icon={faUser}/>
                                 </span>
-                            </div>
-                            <input type="text" className="form-control" placeholder="Username"/>
-                        </div>
-                        <div className="input-group form-group">
-                            <div className="input-group-prepend">
+                                    </div>
+                                    <input type="text"
+                                           className="form-control"
+                                           placeholder="First Name"
+                                           onChange={(event) =>
+                                               this.setState({firstName: event.target.value})}/>
+                                </div>
+
+                                <div className="input-group form-group">
+                                    <div className="input-group-prepend">
+                                <span className="input-group-text">
+                                    <FontAwesomeIcon icon={faUser}/>
+                                </span>
+                                    </div>
+                                    <input type="text"
+                                           className="form-control"
+                                           placeholder="Last Name"
+                                           onChange={(event) =>
+                                               this.setState({lastName: event.target.value})}/>
+                                </div>
+
+                                <div className="input-group form-group">
+                                    <div className="input-group-prepend">
+                                <span className="input-group-text">
+                                    <FontAwesomeIcon icon={faUser}/>
+                                </span>
+                                    </div>
+                                    <input type="text"
+                                           className="form-control"
+                                           placeholder="Email"
+                                           onChange={(event) =>
+                                               this.setState({email: event.target.value})}/>
+                                </div>
+
+                                <div className="input-group form-group">
+                                    <div className="input-group-prepend">
+                                <span className="input-group-text">
+                                    <FontAwesomeIcon icon={faUser}/>
+                                </span>
+                                    </div>
+                                    <select className="form-control"
+                                            onChange={(event) =>
+                                                this.setState({role: event.target.value})}>
+                                        <option value="STUDENT">Student</option>
+                                        <option value="ADMIN">Admin</option>
+                                    </select>
+                                </div>
+
+                                <div className="input-group form-group">
+                                    <div className="input-group-prepend">
                                 <span className="input-group-text">
                                     <FontAwesomeIcon icon={faKey}/>
                                 </span>
-                            </div>
-                            <input type="password" className="form-control" placeholder="Password"/>
-                        </div>
-                        <div className="input-group form-group">
-                            <div className="input-group-prepend">
+                                    </div>
+                                    <input type="password"
+                                           className="form-control"
+                                           placeholder="Password"
+                                           onChange={(event) =>
+                                               this.setState({firstName: event.target.value})}/>
+                                </div>
+
+                                <div className="input-group form-group">
+                                    <div className="input-group-prepend">
                                 <span className="input-group-text">
                                     <FontAwesomeIcon icon={faKey}/>
                                 </span>
+                                    </div>
+                                    <input type="password" className="form-control" placeholder="Confirm Password"/>
+                                </div>
+
+                                <div className="form-group">
+                                    <Link to={"/search"}>
+                                        <button className="btn float-right login_btn"
+                                        // onClick={() => this.props.createUser(this.state)}
+                                        >
+                                            Sign Up
+                                        </button>
+                                    </Link>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="card-footer">
+                            <div className="d-flex justify-content-center links">
+                                Have an account?<a href="/login">Login</a>
                             </div>
-                            <input type="password" className="form-control" placeholder="Confirm Password"/>
                         </div>
-                        <div className="form-group">
-                            <Link to={"/search"}>
-                                <input type="submit" value="Sign Up" className="btn float-right login_btn"/>
-                            </Link>
-                        </div>
-                    </form>
-                </div>
-                <div className="card-footer">
-                    <div className="d-flex justify-content-center links">
-                        Have an account?<a href="/login">Login</a>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+        )
+    }
+}
+
 
 export default SignUpComponent
