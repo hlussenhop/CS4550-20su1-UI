d8a199397e2fe82a1482c329a965ff1dbc8b979d
sophiaw789 <sophialiwon@gmail.com>
2020-06-17 15:10:25 -0400

    login and UserService

---
+++ src/services/UserService.js
@@ -0,0 +1,17 @@
+const login = () => {
+    return fetch("http://neustudyserver.herokuapp.com/api/login", {
+        body: JSON.stringify({
+            username: this.state.username,
+            password: this.state.password
+        }),
+        headers: {
+            'content-type': 'application/json'
+        },
+        method: 'POST',
+        credentials: "include"
+    }).then(response => response.json())
+}
+
+export default {
+    login
+}
