05fface085d492ce4eb6aa7c00d3195c77dd511c
sophiaw789 <sophialiwon@gmail.com>
2020-06-19 04:30:07 -0400

    adding update

--- src/components/ProfileComponents/EditProfileComponent.js
+++ src/components/ProfileComponents/EditProfileComponent.js
@@ -1,14 +1,15 @@
 import React from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faKey,
     faUser,
     faPhone,
     faCalendarDay,
     faFileAlt,
-   faGlobe
+    faGlobe
 } from "@fortawesome/free-solid-svg-icons";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import UserService from "../../services/UserService";
 
 class EditProfileComponent extends React.Component {
 
@@ -19,7 +20,17 @@ class EditProfileComponent extends React.Component {
         dob: '',
         bio: '',
         location: ''
+    }
 
+    update = () => {
+        UserService.update()
+            .catch(e => {
+                this.props.history.push("/editProfile")
+            })
+            .then(currentUser => {
+                if (currentUser)
+                    this.props.history.push("/profile")
+            })
     }
 
     render() {
@@ -34,38 +45,38 @@ class EditProfileComponent extends React.Component {
                             <form>
                                 <div className="input-group form-group">
                                     <div className="input-group-prepend">
-                                <span className="input-group-text">
-                                    <FontAwesomeIcon icon={faUser}/>
-                                </span>
+                                        <span className="input-group-text">
+                                            <FontAwesomeIcon icon={faUser} />
+                                        </span>
                                     </div>
                                     <input className="form-control"
-                                           id="emailFld"
-                                           type="email"
-                                           placeholder="email@gmail.com"
-                                           onChange={(event) =>
-                                               this.setState({email: event.target.value})}
+                                        id="emailFld"
+                                        type="email"
+                                        placeholder="email@gmail.com"
+                                        onChange={(event) =>
+                                            this.setState({ email: event.target.value })}
                                     />
                                 </div>
                                 <div className="input-group form-group">
                                     <div className="input-group-prepend">
-                                <span className="input-group-text">
-                                    <FontAwesomeIcon icon={faKey}/>
-                                </span>
+                                        <span className="input-group-text">
+                                            <FontAwesomeIcon icon={faKey} />
+                                        </span>
                                     </div>
                                     <input className="form-control"
-                                           id="passwordFld"
-                                           type="password"
-                                           placeholder="Password"
-                                           onChange={(event) =>
-                                               this.setState({password: event.target.value})}
+                                        id="passwordFld"
+                                        type="password"
+                                        placeholder="Password"
+                                        onChange={(event) =>
+                                            this.setState({ password: event.target.value })}
                                     />
                                 </div>
 
                                 <div className="input-group form-group">
                                     <div className="input-group-prepend">
-                                <span className="input-group-text">
-                                    <FontAwesomeIcon icon={faFileAlt}/>
-                                </span>
+                                        <span className="input-group-text">
+                                            <FontAwesomeIcon icon={faFileAlt} />
+                                        </span>
                                     </div>
                                     <input
                                         className="form-control"
@@ -73,15 +84,15 @@ class EditProfileComponent extends React.Component {
                                         type="text"
                                         placeholder="Description"
                                         onChange={(event) =>
-                                            this.setState({bio: event.target.value})}
+                                            this.setState({ bio: event.target.value })}
                                     />
                                 </div>
 
                                 <div className="input-group form-group">
                                     <div className="input-group-prepend">
-                                <span className="input-group-text">
-                                    <FontAwesomeIcon icon={faPhone}/>
-                                </span>
+                                        <span className="input-group-text">
+                                            <FontAwesomeIcon icon={faPhone} />
+                                        </span>
                                     </div>
                                     <input
                                         className="form-control"
@@ -90,47 +101,45 @@ class EditProfileComponent extends React.Component {
                                         pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
                                         placeholder="(123) 456-7890"
                                         onChange={(event) =>
-                                            this.setState({phoneNumber: event.target.value})}
+                                            this.setState({ phoneNumber: event.target.value })}
                                     />
                                 </div>
 
                                 <div className="input-group form-group">
                                     <div className="input-group-prepend">
-                                <span className="input-group-text">
-                                    <FontAwesomeIcon icon={faGlobe}/>
-                                </span>
+                                        <span className="input-group-text">
+                                            <FontAwesomeIcon icon={faGlobe} />
+                                        </span>
                                     </div>
                                     <input className="form-control"
-                                           id="locationFld"
-                                           type="text"
-                                           placeholder="Location"
-                                           onChange={(event) =>
-                                               this.setState({location: event.target.value})}
+                                        id="locationFld"
+                                        type="text"
+                                        placeholder="Location"
+                                        onChange={(event) =>
+                                            this.setState({ location: event.target.value })}
                                     />
                                 </div>
 
                                 <div className="input-group form-group">
                                     <div className="input-group-prepend">
-                                <span className="input-group-text">
-                                    <FontAwesomeIcon icon={faCalendarDay}/>
-                                </span>
+                                        <span className="input-group-text">
+                                            <FontAwesomeIcon icon={faCalendarDay} />
+                                        </span>
                                     </div>
                                     <input className="form-control"
-                                           id="dobFld"
-                                           type="date"
-                                           placeholder="YYYY-MM-DD"
-                                           onChange={(event) =>
-                                               this.setState({dob: event.target.value})}
+                                        id="dobFld"
+                                        type="date"
+                                        placeholder="YYYY-MM-DD"
+                                        onChange={(event) =>
+                                            this.setState({ dob: event.target.value })}
                                     />
                                 </div>
 
                                 <div className="form-group">
                                     <Link to={"/profile"}>
                                         <button className="btn float-right login_btn"
-                                                id="updateBtn"
-                                            // onClick={() =>this.props.updateUser(this.state.email, this.state.password,
-                                            //     this.state.phoneNumber, this.state.dob, this.state.bio, this.state.location)}
-                                        >
+                                            id="updateBtn"
+                                        onClick={() =>this.update()}>
                                             Update
                                         </button>
                                     </Link>
