76718ebfeaa4620af3509974ddc95e349ace957a
sophiacrennan <crennan.s@husky.neu.edu>
2020-06-19 03:17:11 -0700

    Have bio and location autogenerate in the fields

--- src/components/ProfileComponents/EditProfileComponent.js
+++ src/components/ProfileComponents/EditProfileComponent.js
@@ -17,8 +17,6 @@ class EditProfileComponent extends React.Component {
         username: '',
         email: '',
         password: '',
-        // phoneNumber: '',
-        // dob: '',
         bio: '',
         location: ''
 
@@ -30,7 +28,9 @@ class EditProfileComponent extends React.Component {
                 this.setState({
                     username: currentUser.username,
                     email: currentUser.email,
-                    password: currentUser.password
+                    password: currentUser.password,
+                    bio: currentUser.bio,
+                    location: currentUser.location
                 })
             })
     }
@@ -45,6 +45,24 @@ class EditProfileComponent extends React.Component {
                         </div>
                         <div className="card-body">
                             <form>
+
+                                <div className="input-group form-group">
+                                    <div className="input-group-prepend">
+                                <span className="input-group-text">
+                                    <FontAwesomeIcon icon={faUser}/>
+                                </span>
+                                    </div>
+                                    <input readOnly
+                                           className="form-control"
+                                           id="usernameFld"
+                                           type="text"
+                                           placeholder={"Username"}
+                                           value={this.state.username}
+                                           onChange={(event) =>
+                                               this.setState({username: event.target.value})}
+                                    />
+                                </div>
+
                                 <div className="input-group form-group">
                                     <div className="input-group-prepend">
                                 <span className="input-group-text">
@@ -58,22 +76,6 @@ class EditProfileComponent extends React.Component {
                                            value={this.state.email}
                                            onChange={(event) =>
                                                this.setState({email: event.target.value})}
-                                    />
-                                </div>
-
-                                <div className="input-group form-group">
-                                    <div className="input-group-prepend">
-                                <span className="input-group-text">
-                                    <FontAwesomeIcon icon={faUser}/>
-                                </span>
-                                    </div>
-                                    <input className="form-control"
-                                           id="usernameFld"
-                                           type="text"
-                                           placeholder={"Username"}
-                                           value={this.state.username}
-                                           onChange={(event) =>
-                                               this.setState({username: event.target.value})}
                                     />
                                 </div>
 
@@ -104,27 +106,11 @@ class EditProfileComponent extends React.Component {
                                         id="bio"
                                         type="text"
                                         placeholder="Description"
+                                        value={this.state.bio}
                                         onChange={(event) =>
                                             this.setState({bio: event.target.value})}
                                     />
                                 </div>
-
-                                {/*<div className="input-group form-group">*/}
-                                {/*    <div className="input-group-prepend">*/}
-                                {/*<span className="input-group-text">*/}
-                                {/*    <FontAwesomeIcon icon={faPhone}/>*/}
-                                {/*</span>*/}
-                                {/*    </div>*/}
-                                {/*    <input*/}
-                                {/*        className="form-control"*/}
-                                {/*        id="phoneFld"*/}
-                                {/*        type="tel"*/}
-                                {/*        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"*/}
-                                {/*        placeholder="(123) 456-7890"*/}
-                                {/*        onChange={(event) =>*/}
-                                {/*            this.setState({phoneNumber: event.target.value})}*/}
-                                {/*    />*/}
-                                {/*</div>*/}
 
                                 <div className="input-group form-group">
                                     <div className="input-group-prepend">
@@ -136,26 +122,12 @@ class EditProfileComponent extends React.Component {
                                            id="locationFld"
                                            type="text"
                                            placeholder="Location"
+                                           value={this.state.location}
                                            onChange={(event) =>
                                                this.setState({location: event.target.value})}
                                     />
                                 </div>
-
-                                {/*<div className="input-group form-group">*/}
-                                {/*    <div className="input-group-prepend">*/}
-                                {/*<span className="input-group-text">*/}
-                                {/*    <FontAwesomeIcon icon={faCalendarDay}/>*/}
-                                {/*</span>*/}
-                                {/*    </div>*/}
-                                {/*    <input className="form-control"*/}
-                                {/*           id="dobFld"*/}
-                                {/*           type="date"*/}
-                                {/*           placeholder="YYYY-MM-DD"*/}
-                                {/*           onChange={(event) =>*/}
-                                {/*               this.setState({dob: event.target.value})}*/}
-                                {/*    />*/}
-                                {/*</div>*/}
-
+                                
                                 <div className="form-group">
                                     <Link to={"/profile"}>
                                         <button className="btn float-right login_btn"
